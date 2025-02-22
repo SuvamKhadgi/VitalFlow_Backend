6997f6a67386be89cee58e37ab49d6f7d5edd4c9
SuvamKhadgi <appleb986@gmail.com>
2025-02-22 15:41:40 +0545

    made new api for devlopement

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const express = require('express')
 const connectDB = require("./config/db")
-const UserRouter = require("./routes/userRoutes")
+const CreadRouter = require("./routes/creadRoutes")
+const ItemRouter = require("./routes/itemsRoutes")
+const CartRouter= require("./routes/cartRoutes")
+const orderRoutes = require("./routes/orderRoutes");
+const dashboardRoutes = require('./routes/dashboardRoutes');
 const app = express();
 
 connectDB();
 app.use(express.json());
-app.use("/api/user", UserRouter);
 
+
+const cors = require('cors'); // Import the cors package
+const corsOptions = {
+    credentials: true,
+    origin: ['http://localhost:5000'] // Whitelist the domains you want to allow
+};
+app.use(cors(corsOptions));
+
+
+app.use("/api/creds", CreadRouter);
+app.use("/api/items", ItemRouter)
+app.use("/api/cart",CartRouter)
+app.use("/api/order", orderRoutes);
+app.use('/api/admin', dashboardRoutes);
+app.use('/uploads', express.static('uploads'));
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
-// app.listen(5002,()=>{
-//     console.log("Server started at 5002")
-// })
-// app.use(express.json())
-// app.post("/data",(req,res)=>{
-//     console.log(req.body)
-//     res.json(req.body)
-// })
