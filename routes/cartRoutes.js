a27313c32b3dfbbae6d0ac1c27bc5093f1af7a5d
SuvamKhadgi <appleb986@gmail.com>
2025-02-22 15:45:54 +0545

    made cart routes

---
+++ routes/cartRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const { findAll, save, findById, deleteById, updateById ,findByUserId,getCartById} = require("../controller/cartController");
+const { authorization } = require("../security/auth");
+
+// const multer = require('multer');
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, 'uploads')
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, file.originalname)
+//     }
+// });
+// const upload = multer({ storage });
+
+router.get("/", findAll);
+router.post("/",  save);
+router.get("/:id",  findById);
+router.get("/user/:id",  findByUserId);
+router.delete("/:id", deleteById);
+router.put("/:id", updateById);
+router.get("/item/:cartId", getCartById);
+module.exports = router;
